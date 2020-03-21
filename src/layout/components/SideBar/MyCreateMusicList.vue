b96fb44b06cd8bd66a924536f06f46e625d2753a
宏智 <7796101035@qq.com>
2020-03-21 23:42:48 +0800

    '不知道gai改了什么'

---
+++ src/layout/components/SideBar/MyCreateMusicList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-submenu index="2">
+      <template slot="title" class="dockTitle">
+        <span>创建的歌单</span>
+      </template>
+
+      <el-menu-item-group>
+        <el-menu-item v-for="(item, index) in musicList" :key="index">
+          <i class="iconfont icon-icon-5" />
+          <span slot="title">{{ item.title }}{{ item.musicListId }}</span>
+        </el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyCreateMusicList',
+  data() {
+    return {
+      musicList: [
+        {
+          title: '歌单1',
+          musicListId: 1
+        },
+        {
+          title: '歌单2',
+          musicListId: 2
+        },
+        {
+          title: '歌单3',
+          musicListId: 3
+        },
+        {
+          title: '歌单4',
+          musicListId: 4
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/layout/components/SideBar/Side.vue
+++ src/layout/components/SideBar/Side.vue
@@ -1,37 +1,64 @@
 <template>
   <div>
-    <el-menu background-color="#e9e8e9" active-text-color="#409eff" default-active="1">
-      <el-menu-item>
+    <!-- <el-menu background-color="#e9e8e9" active-text-color="#409eff" default-active="1">
+      <el-menu-item index="1">
         <i class="iconfont icon-icon-5" />
         <span slot="title">发现音乐</span>
       </el-menu-item>
-      <el-menu-item>
+      <el-menu-item index="2">
         <i class="iconfont icon-icon-1" />
         <span slot="title">私人FM</span>
       </el-menu-item>
-      <el-menu-item>
+      <el-menu-item index="3">
         <i class="iconfont icon-icon-5" />
         <span slot="title">视频</span>
       </el-menu-item>
-      <el-menu-item>
+      <el-menu-item index="4">
         <i class="iconfont icon-icon-5" />
         <span slot="title">朋友</span>
       </el-menu-item>
-    </el-menu>
+
+      <el-menu-item index="5-1">
+        <i class="iconfont icon-icon-5" />
+        <span slot="title">下载管理</span>
+      </el-menu-item>
+      <el-menu-item index="5-2">
+        <i class="iconfont icon-icon-5" />
+        <span slot="title">我的音乐云盘</span>
+      </el-menu-item>
+      <el-menu-item index="5-3">
+        <i class="iconfont icon-icon-5" />
+        <span slot="title">我的电台</span>
+      </el-menu-item>
+      <el-menu-item index="5-4">
+        <i class="iconfont icon-icon-5" />
+        <span slot="title">我的收藏</span>
+      </el-menu-item>
+
+      <MyCreateMusicList />
+    </el-menu>-->
   </div>
 </template>
 
 <script>
+// import MyCreateMusicList from './MyCreateMusicList'
 export default {
   name: 'Side',
+  components: {
+    // MyCreateMusicList
+  },
   data: function() {
     return {}
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
+.dockTitle {
+  text-align: left;
+}
 .el-menu-item {
+  text-align: left;
   i {
     font-size: 22px;
     color: #333;
